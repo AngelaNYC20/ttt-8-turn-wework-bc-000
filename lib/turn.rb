61d77b2960d7aa158caa3c25523f1ae042324d95
Angela A <ranran.wang@wework.com>
2018-02-23 01:32:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,8 +17,8 @@ def turn(board)
    index = input_to_index(input)
    
    if valid_move?(board,index)
-   move(board, index, "O")
-   display_board(board)
+     move(board, index)
+     display_board(board)
   
    else 
    turn(board)
