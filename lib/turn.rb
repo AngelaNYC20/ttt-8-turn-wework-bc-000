496189c2ef4d777743e1546890b745469d40c988
Angela A <ranran.wang@wework.com>
2018-02-23 01:34:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,13 +16,12 @@ def turn(board)
    input = gets.strip
    index = input_to_index(input)
    
-   if valid_move?(board,index)
+  if valid_move?(board,index)
      move(board, index)
      display_board(board)
-  
-   else 
-   turn(board)
-   end 
+  else 
+    turn(board)
+  end 
 end 
 
 
